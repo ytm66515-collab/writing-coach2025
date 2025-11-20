905d2bdb66dc18c3d023f7093e9585edc5cb9c67
ytm66515-collab <ytm66515@gmail.com>
2025-11-20 20:24:19 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
+    // Avoid crashing in browser by defining an empty process.env object
+    // Our code already checks for typeof process !== 'undefined' so this is safe
     'process.env': {}
   }
 })
